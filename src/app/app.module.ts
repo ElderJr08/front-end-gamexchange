6edda971b9d30b7e3142349bdc0f9babe823b9f7
Elder Junior <elderjunior08@gmail.comr>
2018-09-21 18:11:04 -0300

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 import { TabelasComponent } from './home/tabelas/tabelas.component';
 
 @NgModule({
@@ -13,7 +16,9 @@ import { TabelasComponent } from './home/tabelas/tabelas.component';
     TabelasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
